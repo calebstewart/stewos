ac61d08c1c71f2989afafea8ef39933cd47be063
Caleb Stewart <caleb.stewart94@gmail.com>
2026-05-15 12:58:00 -0500

    Configure nat/dns stuff for virtualization

--- modules/nixos/virtualisation/default.nix
+++ modules/nixos/virtualisation/default.nix
@@ -12,8 +12,14 @@ in
   options.stewos.virtualisation.enable = lib.mkEnableOption "virtualisation";
 
   config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      virt-manager
+    ];
+
     virtualisation.libvirtd = {
       enable = true;
+      nss.enable = true;
+      nss.enableGuest = true;
 
       qemu = {
         package = pkgs.qemu_kvm;
@@ -29,11 +35,23 @@ in
 
     # Allow communication from VM to host over common ephemeral ports
     # This is normally things like updog for transferring files.
-    networking.firewall.interfaces."virbr0".allowedTCPPortRanges = [
-      {
-        from = 8000;
-        to = 10000;
-      }
-    ];
+    networking.firewall.interfaces."virbr0" = {
+      allowedTCPPortRanges = [
+        {
+          from = 8000;
+          to = 10000;
+        }
+      ];
+
+      allowedUDPPorts = [
+        53
+        67
+      ];
+    };
+
+    networking.nat = {
+      enable = true;
+      internalInterfaces = [ "virbr0" ];
+    };
   };
 }
