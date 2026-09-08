33910db1aa5a18b5687f52db385da0f089531dc6
Caleb Stewart <caleb.stewart94@gmail.com>
2026-09-07 22:13:40 -0500

    Added base windows configuration to gaming-windows

--- hosts/gaming-windows/configuration.nix
+++ hosts/gaming-windows/configuration.nix
@@ -8,7 +8,50 @@
     "Git.Git"
   ];
 
-  winpkgs.files."%USERPROFILE%/test.txt".text = "hello world\n";
+  winpkgs.theme.mode = "dark";
+  winpkgs.developer.developerMode = true;
+  winpkgs.keyboard.stickyKeysShortcut = false;
+
+  # Configure the task bar
+  winpkgs.taskbar = {
+    alignment = "left";
+    searchBox = "hidden";
+    widgets = false;
+    chat = false;
+    taskViewButton = false;
+    showOnAllDisplays = true;
+    combineButtons = "whenFull";
+  };
+
+  # Configure Windows Explorer
+  winpkgs.explorer = {
+    contextMenu = "classic";
+    showHiddenFiles = true;
+    showFileExtensions = true;
+    showProtectedOsFiles = true;
+    launchTo = "home";
+    compactMode = true;
+    expandToCurrentFolder = true;
+    hideDrivesWithNoMedia = true;
+    showSyncProviderNotifications = false;
+  };
+
+  # Configure powershell
+  winpkgs.powershell = {
+    ensure = true;
+    upgrade = true;
+  };
+
+  # Configure Windows "privacy" options
+  winpkgs.privacy = {
+    advertisingId = false;
+    suggestedContent = false;
+    suggestedApps = false;
+    tips = false;
+    webSearchInStart = false;
+    activityFeed = false;
+    telemetry = "required";
+  };
 
   # Where this flake is checked out on the Windows side, so `winpkgs plan` etc.
   # work from any Windows terminal without naming it.
